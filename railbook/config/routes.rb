b53e4cdddfe72ea2f53c42055f6e0da0b8ce15bf
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-19 11:30:06 +0900

    change hello/index route

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello/index', to: 'hello#index'
-  get 'hello/view', to: 'hello#view'
+  get 'hello/', to: 'hello#index'
+  get 'hello/view/', to: 'hello#view'
 end
