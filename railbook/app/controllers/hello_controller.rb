b85512c37b8d356ff540a148060465a49a1427e3
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-19 11:20:02 +0900

    add era

--- railbook/app/controllers/hello_controller.rb
+++ railbook/app/controllers/hello_controller.rb
@@ -2,4 +2,11 @@ class HelloController < ApplicationController
   def index
     render plain: 'hello world.'
   end
+
+  def view
+    @title = 'hello Ruby on Rails 5.'
+    @price = 3600
+    @tax = 1.08
+    render 'hello/view'
+  end
 end
