7cece9b03800f836ae4763311d5f5ceda63eef2c
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-19 17:34:53 +0900

    create books CRUD page by scaffold

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170419025957) do
+ActiveRecord::Schema.define(version: 20170419082340) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
