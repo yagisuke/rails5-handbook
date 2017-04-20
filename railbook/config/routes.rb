3bd66db94eee21ad7371ca21e4c109a8437b5f53
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-04-20 10:47:04 +0900

    route ファイルをリファクタ

--- railbook/config/routes.rb
+++ railbook/config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   resources :users
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'hello/', to: 'hello#index'
-  get 'hello/view/', to: 'hello#view'
-  get 'hello/nothing/', to: 'hello#nothing'
-  get 'hello/list/', to: 'hello#list'
-  get 'hello/app_var/', to: 'hello#app_var'
+  get 'hello', to: 'hello#index'
+  get 'hoge/index', to: 'hello#index'
+  get 'hello/view', to: 'hello#view'
+  get 'hello/nothing'
+  get 'hello/list'
+  get 'hello/app_var'
+
 end
