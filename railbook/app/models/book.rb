d0cfdf05be7f67bb812d6b7778b6d02df0c83174
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-05-02 22:55:34 +0900

    validate sample

--- railbook/app/models/book.rb
+++ railbook/app/models/book.rb
@@ -1,4 +1,17 @@
 class Book < ApplicationRecord
+  validates :isbn,
+    presence: true,
+    uniqueness: true,
+    length: { is: 17 },
+    format: { with: /\A[0-9]{3}-[0-9]{1}-[0-9]{3,5}-[0-9]{4}-[0-9X]{1}\z/ }
+  validates :title,
+    presence: true,
+    length: { minimum: 1, maximum: 100 }
+  validates :price,
+    numericality: { only_integer: true, less_than: 10000 }
+  validates :publish,
+    inclusion: { in: ['技術評論社', '秀和システム', '日経BP社', '翔泳社', 'ソシム']}
+
   scope :gihyo, -> { where(publish: '技術評論社') }
   scope :newer, -> { order(published: :desc) }
   scope :top10, -> { newer.limit(10) }
