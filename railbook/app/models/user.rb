e607933d9c20eba41330b11b3883d22ee2440eed
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-05-04 13:22:48 +0900

    confirmation sample

--- railbook/app/models/user.rb
+++ railbook/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :agreement,
     acceptance: true
-    
+  validates :email, confirmation: true
+
   has_one :author
   has_many :reviews
   has_many :books, through: :reviews
