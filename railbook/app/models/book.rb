88e04b71ef35856e1ce7a79b6a5d6da529d65721
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-05-04 14:10:25 +0900

    allow_blank 必須チェックでエラーとなるので、他のバリデーションはスキップするようにする

--- railbook/app/models/book.rb
+++ railbook/app/models/book.rb
@@ -1,12 +1,12 @@
 class Book < ApplicationRecord
   validates :isbn,
     presence: true,
-    uniqueness: true,
-    length: { is: 17 },
-    format: { with: /\A[0-9]{3}-[0-9]{1}-[0-9]{3,5}-[0-9]{4}-[0-9X]{1}\z/ }
+    uniqueness: { allow_blank: true },
+    length: { is: 17, allow_blank: true },
+    format: { with: /\A[0-9]{3}-[0-9]{1}-[0-9]{3,5}-[0-9]{4}-[0-9X]{1}\z/, allow_blank: true }
   validates :title,
     presence: true,
-    length: { minimum: 1, maximum: 100 }
+    length: { minimum: 1, maximum: 100, allow_blank: true }
   validates :price,
     numericality: { only_integer: true, less_than: 10000 }
   validates :publish,
