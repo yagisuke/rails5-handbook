4e350f4529980e6e28aee04c3f1b2bb0b7988ab4
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-05-28 23:45:30 +0900

    モデルで認証機能を有効化

--- railbook/app/models/user.rb
+++ railbook/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  
   validates :email, presence: { if: '!dm.blank?' }
   validates :email, confirmation: { on: :create }
   validates :agreement,
