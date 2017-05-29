de3f3b2771410deb406c27b8ce687efef5427780
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-05-30 01:35:16 +0900

    auth function

--- railbook/app/controllers/login_controller.rb
+++ railbook/app/controllers/login_controller.rb
@@ -1,2 +1,19 @@
 class LoginController < ApplicationController
+  # [ログイン]ボタンのクリック時に実行されるアクション
+  def auth
+    # 入力値にしたがってユーザ情報を取得
+    usr = User.find_by(username: params[:username])
+    # ユーザ情報が存在し認証にに成功したら
+    if usr && usr.authenticate(params[:password]) then
+      # 成功した場合はid値をセッションに設定し、元々の要求ページにリダイレクト
+      reset_session
+      session[:usr] = usr.id
+      redirect_to params[:refer]
+    else
+      # 失敗した場合はfalse[:referer]を再セットし、ログインページを再描画
+      flash.now[:referer] = params[:referer]
+      @error = 'ユーザ/パスワードが間違っています'
+      render 'index'
+    end
+  end
 end
