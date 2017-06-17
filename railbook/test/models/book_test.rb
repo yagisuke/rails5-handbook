2d7e3db502f3e7857b32b7ea3be6fe20d9d33c96
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-17 16:03:15 +0900

    lets test

--- railbook/test/models/book_test.rb
+++ railbook/test/models/book_test.rb
@@ -12,4 +12,18 @@ class BookTest < ActiveSupport::TestCase
     })
     assert book.save, 'Failed to save'
   end
+
+  test "book validate" do
+    book = Book.new({
+      isbn: '978-4-7741-44',
+      title: 'Ruby on Rails本格入門',
+      price: 3100,
+      publish: '技術評論社',
+      published: '2017-02-14',
+      dl: false
+    })
+    assert !book.save, 'Failed to validate'
+    assert_equal book.errors.size, 2, 'Failed to validate count'
+    assert book.errors[:isbn].any?, 'Failed to isbn validate'
+  end
 end
