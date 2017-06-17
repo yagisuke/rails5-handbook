d50d236e1d84b417493076ddbd25677e36e28363
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-17 15:56:09 +0900

    lets test

--- railbook/test/models/book_test.rb
+++ railbook/test/models/book_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "book save" do
+    book = Book.new({
+      isbn: '978-4-7741-4466-X',
+      title: 'Ruby on Rails本格入門',
+      price: 3100,
+      publish: '技術評論社',
+      published: '2017-02-14',
+      dl: false
+    })
+    assert book.save, 'Failed to save'
+  end
 end
