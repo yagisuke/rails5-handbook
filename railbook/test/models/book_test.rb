3b58971535ea9b802cc83c9f7d1c69c2e5d9a328
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-18 16:30:39 +0900

    lets test

--- railbook/test/models/book_test.rb
+++ railbook/test/models/book_test.rb
@@ -26,4 +26,12 @@ class BookTest < ActiveSupport::TestCase
     assert_equal book.errors.size, 2, 'Failed to validate count'
     assert book.errors[:isbn].any?, 'Failed to isbn validate'
   end
+
+  test "where method test" do
+    result = Book.find_by(title: '改訂新版JavaScript本格入門')
+    assert_instance_of Book, result, 'result is not instance of Book'
+    assert_equal books(:modernjs).isbn, result.isbn, 'isbn column is wrong.'
+    assert_equal Date.new(2016, 9, 30), result.published,
+      'published column is wrong.'
+  end
 end
