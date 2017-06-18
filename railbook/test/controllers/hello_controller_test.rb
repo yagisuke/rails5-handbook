ab756787674a739a831608509bed395bda1f22ce
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-18 17:12:25 +0900

    lets test

--- railbook/test/controllers/hello_controller_test.rb
+++ railbook/test/controllers/hello_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class HelloControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "list action" do
+    get '/hello/list'
+    assert_equal 10, assigns(:books).length, 'found rows is wrong.'
+    assert_response :success, 'list action failed.'
+    assert_template 'hello/list'
+  end
 end
