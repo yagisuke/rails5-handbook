1a1004b638b57583c155560f1b41e2cdb1b9ec6b
Yusuke Yagishita <yagisuke.0209@gmail.com>
2017-06-19 23:50:54 +0900

    lets test

--- railbook/test/controllers/hello_controller_test.rb
+++ railbook/test/controllers/hello_controller_test.rb
@@ -7,18 +7,4 @@ class HelloControllerTest < ActionDispatch::IntegrationTest
     assert_response :success, 'list action failed.'
     assert_template 'hello/list'
   end
-
-  test "diff check" do
-    assert_difference 'Book.count', 1 do
-      post books_url,
-        params: {
-          book: {
-            isbn: '978-4-7741-4223-0',
-            title: 'Rubyポケットリファレンス',
-            price: 3000,
-            publish: '技術評論社'
-          }
-        }
-    end
-  end
 end
